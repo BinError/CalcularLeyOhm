905aec8c59799452c59b10e6f5764188dfe2ea45
BinError <marcknmt@gmail.com>
2022-12-11 16:40:44 -0300

    mejorado UX y agregados mensajes de navbar

--- main.js
+++ main.js
@@ -41,3 +41,10 @@ function calcularOhm(ohm, amp, vol){
 function llamar(){
     return calcularOhm(formulario[0].Ohm.value,formulario[0].Amp.value,formulario[0].Vol.value)
 }
+
+function quienSoy(){
+    alert("Hola, Soy Ohm")
+}
+function queHago(){
+    alert("Tomaré dos valores para calcular el tercer valor de la ley de Ohm.\n Si me das los Ohmios e Intensidad te dare Voltios. \n Si me das Intensidad y Voltios te daré Ohmios. \n Y si me das Voltios y Ohms te daré Intensidad")
+}
